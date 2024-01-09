83c20c9eb8d67a2c1deab60d800fc8a124d044d1
Emmanuel Ochoa <eorivera95@gmail.com>
2024-01-09 14:25:51 -0600

    feat: routing examples

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>Hello nuxt 3</div>
+  <NuxtLayout>
+    <!--NuxtPage works as router view components-->
+    <header>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="events">Events</NuxtLink></li>
+      </ul>
+    </header>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
